d486fc20c418d7df14887b3fd560aba3e8c4b47a
DARK-PROGRAMMER-I <hammadmalli64@gmail.com>
2022-05-27 15:41:58 +0500

    Food Delivery App * Main Page body

--- lib/home/homePageBody.dart
+++ lib/home/homePageBody.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/utils/colors.dart';
 import 'package:food_delivery_app/widgets/big_text.dart';
+import 'package:food_delivery_app/widgets/small_text.dart';
 
 class HomePageBody extends StatefulWidget {
   const HomePageBody({Key? key}) : super(key: key);
@@ -10,12 +11,14 @@ class HomePageBody extends StatefulWidget {
 }
 
 class _HomePageBodyState extends State<HomePageBody> {
+  PageController pageController = PageController(viewportFraction: 0.8);
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.red,
       height: 320,
       child: PageView.builder(
+        controller: pageController,
           itemCount: 5,
           itemBuilder: (context , position){
         return _buildPageItem(position);
@@ -26,44 +29,57 @@ class _HomePageBodyState extends State<HomePageBody> {
   Widget _buildPageItem(int index){
     return Stack(
           children: [
-            Align(
-              child: Container(
-                      height: 220,
-                      width: 320,
-                      margin: EdgeInsets.only(left: 5, right: 5),
-                      decoration: BoxDecoration(
-                        color: index.isEven ? Colors.purple : Colors.indigo,
-                        borderRadius: BorderRadius.circular(30),
-                        image: DecorationImage(
-                            image: AssetImage('assets/image/food0.png'),
-                            fit: BoxFit.cover
-                        )
-                      ),
-              ),
+            Container(
+                    height: 220,
+                    // width: 320,
+                    margin: EdgeInsets.only(left: 10, right: 10),
+                    decoration: BoxDecoration(
+                      color: index.isEven ? Colors.purple : Colors.indigo,
+                      borderRadius: BorderRadius.circular(25),
+                      image: DecorationImage(
+                          image: AssetImage('assets/image/food0.png'),
+                          fit: BoxFit.cover
+                      )
+                    ),
             ),
             Align(
               alignment: Alignment.bottomCenter,
               child: Container(
                 height: 120,
-                width: 280,
-                margin: EdgeInsets.only(left: 5, right: 5),
+                // width: 280,
+                margin: EdgeInsets.only(left: 30, right: 30, bottom: 20),
                 decoration: BoxDecoration(
                     color: AppColors.mainWhiteColor,
-                    borderRadius: BorderRadius.circular(30),
-                    ),
-                child: Column(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(left: 20, top: 20),
-                      alignment: Alignment.topLeft,
-                        child: BigText('Chinese Food')),
-                    Row(
-                      children: [
+                    borderRadius: BorderRadius.circular(25),
 
-                      ],
                     ),
-                    Row()
-                  ],
+                child: Container(
+                  padding: EdgeInsets.only(left: 15, right: 15, top: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      BigText('Chinese Food'),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: [
+                          Wrap(
+                          children: List.generate(5, (index) => Icon(
+                            Icons.star, color: AppColors.mainColor,size: 12,)),
+                        ),
+                          Wrap(
+                            spacing: 5,
+                            children: [
+
+                              SmallText('4.5', ),
+
+                              SmallText('1287  comments')
+                            ],
+                          )
+
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
